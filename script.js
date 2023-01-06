dcd664ffee8ca94c1af824bea902864789bfc648
mahfuzurmunna <mahfuzurrm07@gmail.com>
2023-01-06 18:30:32 +0600

    forth repo

---
+++ script.js
@@ -0,0 +1,9 @@
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg');
+
+let bg = 0;
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
